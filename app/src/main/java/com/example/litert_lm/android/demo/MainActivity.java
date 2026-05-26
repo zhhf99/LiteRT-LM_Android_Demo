b7dda0c13226e81bfdb083087e5b5b43e076069f
zhhf99 <zhhf99@outlook.com>
2026-05-26 22:40:00 +0800

    finally, all done.

--- app/src/main/java/com/example/litert_lm/android/demo/MainActivity.java
+++ app/src/main/java/com/example/litert_lm/android/demo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.litert_lm.android.demo;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,7 +14,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.litertlmdemo.LiteLlmWrapper;
 import com.google.ai.edge.litertlm.*;
 
 import java.io.ByteArrayOutputStream;
@@ -22,8 +21,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -72,57 +69,68 @@ public class MainActivity extends AppCompatActivity {
         engine = new Engine(engineConfig);
         engine.initialize();
 
+
+        byte[] data = new byte[10 * 1024 * 1024];
+
+        try (InputStream is = getApplicationContext().getAssets().open("test.jpg");
+             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ) {
+            int n;
+            while ((n = is.read(data)) != -1) {
+                buffer.write(data, 0, n);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         ConversationConfig conversationConfig = new ConversationConfig(
-                Contents.Companion.of("你是一个图像场景分类器，服务于盲人辅助导航系统。你的任务是判断输入图片中的场景属于“路口”还是“人行道”。\n" +
-                        "分类规则：\n" +
-                        "如果图像中存在明显的道路交汇、十字路口、丁字路口、车辆通行交叉区域、红绿灯、斑马线与机动车道交汇等特征，输出：路口\n" +
-                        "如果图像主要是人行道、步行街、人行区域、与机动车道无明显交叉的步行路径，输出：人行道\n" +
-                        "输出要求：\n" +
-                        "只能输出“路口”或“人行道”之一，不能输出任何解释、标点、换行、空格或其他文字，不能输出多余内容，不能提及判断过程，不能输出置信度\n" +
-                        "如果无法确定，优先根据是否存在机动车道交汇判断：有交叉车流风险为路口，无明显交叉车流为人行道")
+//                Contents.Companion.of("你是一个图像场景分类器，服务于盲人辅助导航系统。你的任务是判断输入图片中的场景属于“路口”还是“人行道”。\n" +
+//                        "分类规则：\n" +
+//                        "如果图像中存在明显的道路交汇、十字路口、丁字路口、车辆通行交叉区域、红绿灯、斑马线与机动车道交汇等特征，输出：路口\n" +
+//                        "如果图像主要是人行道、步行街、人行区域、与机动车道无明显交叉的步行路径，输出：人行道\n" +
+//                        "输出要求：\n" +
+//                        "只能输出“路口”或“人行道”之一，不能输出任何解释、标点、换行、空格或其他文字，不能输出多余内容，不能提及判断过程，不能输出置信度\n" +
+//                        "如果无法确定，优先根据是否存在机动车道交汇判断：有交叉车流风险为路口，无明显交叉车流为人行道")
+                Contents.Companion.of("简单描述这张图片的内容")
         );
 
         send.setOnClickListener(v -> {
-            try (Conversation conversation = engine.createConversation(conversationConfig);
-                 InputStream is = getApplicationContext().getAssets().open("test.jpg");
-            ) {
+            Conversation conversation = engine.createConversation(conversationConfig);
 
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            conversation.sendMessageAsync(Contents.Companion.of(
+                    new Content.ImageBytes(data)
+            ), new MessageCallback() {
+                private StringBuilder builder = new StringBuilder();
 
-                byte[] data = new byte[10 * 1024 * 1024];
-                int n;
-
-                while ((n = is.read(data)) != -1) {
-                    buffer.write(data, 0, n);
+                @Override
+                public void onMessage(@NonNull Message message) {
+                    Log.d(MainActivity.this.toString(), "onMessage...");
+                    builder.append(message);
+                    runOnUiThread(() -> {
+                        output.setText(builder.toString());
+                    });
                 }
 
-                is.close();
+                @Override
+                public void onDone() {
+                    new Thread(() -> {
+                        //have to close conversation in new thread, try closing directly in this method will get stuck
+                        //and if try to create new conversation when stuck, app will crash
+                        Log.d(MainActivity.this.toString(), "Message Done, cleaning conversation in new thread...");
+                        conversation.close();
+                        Log.d(MainActivity.this.toString(), "Message Done, conversation closed.");
+                    }).start();
+                    Log.d(MainActivity.this.toString(), "onDone after clean in new thread.");
+                }
 
-                conversation.sendMessageAsync(Contents.Companion.of(
-                        new Content.Text("1+1=几")
-                ), new MessageCallback() {
-                    @Override
-                    public void onMessage(@NonNull Message message) {
-                        runOnUiThread(() -> {
-                            output.setText(message.toString());
-                        });
-                    }
+                @Override
+                public void onError(@NonNull Throwable throwable) {
+                    throwable.printStackTrace();
+                }
 
-                    @Override
-                    public void onDone() {
+            }, Collections.emptyMap());
 
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable throwable) {
-
-                    }
-                }, Collections.emptyMap());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         });
-
 
     }
 
